df1da41de20ab6d8c006c9a9f7e058d0f94d4682
jomas <jomasjoao@gmail.com>
2019-12-22 15:36:18 -0300

    Adicionado evento de transição de remoção

--- js/remover.js
+++ js/remover.js
@@ -2,19 +2,21 @@
 
 removerPaciente();
 
-//Função Para remover um paciente
+//Função para remover um paciente
 function removerPaciente(){
 	var tabela = document.querySelector("table");//selecionando a tabela de dados
 
 	//adicionando evento de duplo click
 	tabela.addEventListener("dblclick", function(event) {
 
+		event.target.parentNode.classList.add("fadeOut");
 		/*
 		var alvoEvento = event.target;
 		var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover 
 		paiDoAlvo.remove();
 		*/
-
-	 event.target.parentNode.remove();// função que remove a TR
+		setTimeout(function() {
+        	event.target.parentNode.remove(); // função que remove a TR
+    	}, 500);
 	});
 }
